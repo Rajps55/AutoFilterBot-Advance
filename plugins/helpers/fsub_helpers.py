539a5833f261c5c216b774caec40e9d024cd5af2
Rajps55 <hackinggm69@gmail.com>
2024-10-06 01:41:25 +0530

    Update fsub_helpers.py

--- plugins/helpers/fsub_helpers.py
+++ plugins/helpers/fsub_helpers.py
@@ -1,54 +1,52 @@
 from pyrogram import enums, Client
-from ..helpers.fsub_helpers import is_admin, can_ban_members, can_change_info
 
 
 
-async def is_owner(chat_id: int, user_id: int):
-    async for x in Client.get_chat_members(chat_id):
-        if x.status == enums.ChatMemberStatus.OWNER:
-             if x.user.id == user_id:
-                 return True
-             else: return False
 
-async def is_admin(chat_id: int, user_id: int):
-     admin = await Client.get_chat_member(chat_id, user_id)
-     if admin.privileges:
-         return True
-     return False
+async def is_owner(client: Client, chat_id: int, user_id: int):
+    async for x in client.get_chat_members(chat_id):
+        if x.status == enums.ChatMemberStatus.OWNER and x.user.id == user_id:
+            return True
+    return False
 
-async def can_ban_members(chat_id: int, user_id: int):
-     admin = await Client.get_chat_member(chat_id, user_id)
-     if admin.privileges.can_restrict_members:
-         return True
-     return False
+async def is_admin(client: Client, chat_id: int, user_id: int):
+    try:
+        admin = await client.get_chat_member(chat_id, user_id)
+        return admin.privileges is not None
+    except Exception:
+        return False
 
-async def can_pin_messages(chat_id: int, user_id: int):
-     admin = await Client.get_chat_member(chat_id, user_id)
-     if admin.privileges.can_pin_messages:
-         return True
-     return False
+async def can_ban_members(client: Client, chat_id: int, user_id: int):
+    try:
+        admin = await client.get_chat_member(chat_id, user_id)
+        return admin.privileges.can_restrict_members if admin.privileges else False
+    except Exception:
+        return False
 
-async def can_delete_messages(chat_id: int, user_id: int):
-     admin = await Client.get_chat_member(chat_id, user_id)
-     if admin.privileges.can_delete_messages:
-         return True
-     return False
+async def can_pin_messages(client: Client, chat_id: int, user_id: int):
+    try:
+        admin = await client.get_chat_member(chat_id, user_id)
+        return admin.privileges.can_pin_messages if admin.privileges else False
+    except Exception:
+        return False
 
-async def can_promote_members(chat_id: int, user_id: int):
-     admin = await Client.get_chat_member(chat_id, user_id)
-     if admin.privileges.can_promote_members:
-         return True
-     return False
+async def can_delete_messages(client: Client, chat_id: int, user_id: int):
+    try:
+        admin = await client.get_chat_member(chat_id, user_id)
+        return admin.privileges.can_delete_messages if admin.privileges else False
+    except Exception:
+        return False
 
-async def can_promote_members(chat_id: int, user_id: int):
-     admin = await Client.get_chat_member(chat_id, user_id)
-     if admin.privileges.can_promote_members:
-         return True
-     return False
+async def can_promote_members(client: Client, chat_id: int, user_id: int):
+    try:
+        admin = await client.get_chat_member(chat_id, user_id)
+        return admin.privileges.can_promote_members if admin.privileges else False
+    except Exception:
+        return False
 
-async def can_change_info(chat_id: int, user_id: int):
-     admin = await Client.get_chat_member(chat_id, user_id)
-     if admin.privileges.can_change_info:
-         return True
-     return False
-
+async def can_change_info(client: Client, chat_id: int, user_id: int):
+    try:
+        admin = await client.get_chat_member(chat_id, user_id)
+        return admin.privileges.can_change_info if admin.privileges else False
+    except Exception:
+        return False
