1437a53a7ca59284fea9874506e7595f9098be79
Rajps55 <hackinggm69@gmail.com>
2024-10-05 20:31:46 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,37 +1,37 @@
-import os
-import time
-import asyncio
-import uvloop
+import logging
+import logging.config
 
-# pyrogram imports
-from pyrogram import types
-from pyrogram import Client
-from pyrogram.errors import FloodWait
+# Get logging configurations
+logging.config.fileConfig('logging.conf')
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("cinemagoer").setLevel(logging.ERROR)
 
-# aiohttp imports
-from aiohttp import web
-from typing import Union, Optional, AsyncGenerator
-
-# local imports
-from info import LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL, ADMINS, DATABASE_URL
-from utils import temp, get_readable_time
-
-# pymongo and database imports
-from database.users_chats_db import db
+from pyrogram import Client, __version__
+from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from aiohttp import web
+from database.users_chats_db import db
+from web import web_server
+from info import SESSION, LOG_CHANNEL, API_ID, API_HASH, BOT_TOKEN, PORT, BIN_CHANNEL
+from utils import temp
+from typing import Union, Optional, AsyncGenerator
+from pyrogram import types
+import time, os
+from pyrogram.errors import AccessTokenExpired, AccessTokenInvalid
 
-uvloop.install()
 
 class Bot(Client):
+
     def __init__(self):
         super().__init__(
-            name='Auto_Filter_Bot',
+            name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins={"root": "plugins"}
+            workers=50,
+            plugins={"root": "plugins"},
+            sleep_threshold=5,
         )
 
     async def start(self):
@@ -39,14 +39,11 @@ class Bot(Client):
         b_users, b_chats = await db.get_banned()
         temp.BANNED_USERS = b_users
         temp.BANNED_CHATS = b_chats
-        client = MongoClient(DATABASE_URL, server_api=ServerApi('1'))
         try:
-            client.admin.command('ping')
-            print("Pinged your deployment. You successfully connected to MongoDB!")
-        except Exception as e:
-            print("Something Went Wrong While Connecting To Database!", e)
+            await super().start()
+        except (AccessTokenExpired, AccessTokenInvalid):
+            logging.error("Your BOT_TOKEN revoke and add again, exiting now")
             exit()
-        await super().start()
         if os.path.exists('restart.txt'):
             with open("restart.txt") as file:
                 chat_id, msg_id = map(int, file)
@@ -62,32 +59,33 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         username = '@' + me.username
-        print(f"{me.first_name} is started now 🤗")
-        #groups = await db.get_all_chats_count()
-        #for grp in groups:
-            #await save_group_settings(grp['id'], 'fsub', "")
-        app = web.AppRunner(web_app)
+        app = web.AppRunner(await web_server())
         await app.setup()
         await web.TCPSite(app, "0.0.0.0", PORT).start()
+        logging.info(f"\n\n{username} is started!\n\n")
         try:
             await self.send_message(chat_id=LOG_CHANNEL, text=f"<b>{me.mention} Restarted! 🤖</b>")
         except:
-            print("Error - Make sure bot admin in LOG_CHANNEL, exiting now")
+            logging.error("Make sure bot admin in LOG_CHANNEL, exiting now")
             exit()
         try:
             m = await self.send_message(chat_id=BIN_CHANNEL, text="Test")
             await m.delete()
         except:
-            print("Error - Make sure bot admin in BIN_CHANNEL, exiting now")
+            logging.error("Make sure bot admin in BIN_CHANNEL, exiting now")
             exit()
-        for admin in ADMINS:
-            await self.send_message(chat_id=admin, text="<b>✅ ʙᴏᴛ ʀᴇsᴛᴀʀᴛᴇᴅ</b>")
+
 
     async def stop(self, *args):
         await super().stop()
-        print("Bot Stopped! Bye...")
-
-    async def iter_messages(self: Client, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:
+        logging.info("Bot stopped! Bye...")
+    
+    async def iter_messages(
+        self,
+        chat_id: Union[int, str],
+        limit: int,
+        offset: int = 0,
+    ) -> Optional[AsyncGenerator["types.Message", None]]:
         """Iterate through a chat sequentially.
         This convenience method does the same as repeatedly calling :meth:`~pyrogram.Client.get_messages` in a loop, thus saving
         you from the hassle of setting up boilerplate code. It is useful for getting the whole chat messages with a
@@ -108,7 +106,7 @@ class Bot(Client):
             ``Generator``: A generator yielding :obj:`~pyrogram.types.Message` objects.
         Example:
             .. code-block:: python
-                async for message in app.iter_messages("pyrogram", 1000, 100):
+                for message in app.iter_messages("pyrogram", 1, 15000):
                     print(message.text)
         """
         current = offset
@@ -120,6 +118,7 @@ class Bot(Client):
             for message in messages:
                 yield message
                 current += 1
+
 
 app = Bot()
 app.run()
